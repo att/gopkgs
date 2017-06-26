db4684189643cf5d5b6a8d7e6916987b21b28560
Scott Daniels <daniels@research.att.com>
2017-06-26 14:15:33 -0400

    Update tokeniser tests

--- token/tokenise_test.go
+++ token/tokenise_test.go
@@ -282,3 +282,14 @@ func TestUnique( t *testing.T ) {
 	fmt.Fprintf( os.Stderr, "\n" )
 }
 
+func TestJsonParsing( t *testing.T ) {
+	//s := `{"_context_domain": null, "_msg_id": "01cc4c0579e64b2798ecd028a8c206aa", "_context_quota_class": null, "_context_read_only": false, "_context_request_id": "req-896b224d-a47f-43a9-abbb-d9ef8988cb21", "_context_service_catalog": [], "args": {"objmethod": "save", "args": [], "objinst": {"nova_object.version": "1.5", "nova_object.changes": ["instance_uuid", "network_info"], "nova_object.name": "InstanceInfoCache", "nova_object.data": {"instance_uuid": "80f274fc-8854-469e-8571-abc9caf73ed1", "network_info": "[{\"profile\": {}, \"ovs_interfaceid\": \"3562e20a-4441-490e-bf67-ae1c7b0dc66d\", \"preserve_on_delete\": false, \"network\": {\"bridge\": \"br-int\", \"subnets\": [{\"ips\": [{\"meta\": {}, \"version\": 4, \"type\": \"fixed\", \"floating_ips\": [], \"address\": \"10.7.4.50\"}], \"version\": 4, \"meta\": {\"dhcp_server\": \"10.7.0.2\"}, \"dns\": [{\"meta\": {}, \"version\": 4, \"type\": \"dns\", \"address\": \"135.207.177.11\"}, {\"meta\": {}, \"version\": 4, \"type\": \"dns\", \"address\": \"135.207.179.11\"}], \"routes\": [], \"cidr\": \"10.7.0.0/16\", \"gateway\": {\"meta\": {}, \"version\": 4, \"type\": \"gateway\", \"address\": \"10.7.0.1\"}}], \"meta\": {\"injected\": false, \"tenant_id\": \"65c3e5ee5ee0428caa5e5275c58ead61\"}, \"id\": \"e174ae6a-ef11-45e4-b888-add340e98c4f\", \"label\": \"cloudqos-private\"}, \"devname\": \"tap3562e20a-44\", \"vnic_type\": \"normal\", \"qbh_params\": null, \"meta\": {}, \"details\": {\"port_filter\": true, \"ovs_hybrid_plug\": true}, \"address\": \"fa:16:3e:0a:cf:80\", \"active\": true, \"type\": \"ovs\", \"id\": \"3562e20a-4441-490e-bf67-ae1c7b0dc66d\", \"qbg_params\": null}]"}, "nova_object.namespace": "nova"}, "kwargs": {"update_cells": false}}, "_unique_id": "3083ecbd1a7042f1b663d181a6f9e9bc", "_context_resource_uuid": null, "_context_instance_lock_checked": false, "_context_user": null, "_context_user_id": null, "_context_project_name": null, "_context_read_deleted": "no", "_context_user_identity": "- - - - -", "_reply_q": "reply_8305989e7b6649d6b90007ee5e4aa14c", "_context_auth_token": null, "_context_show_deleted": false, "_context_tenant": null, "_context_roles": [], "_context_is_admin": true, "version": "2.0", "_context_project_id": null, "_context_project_domain": null, "_context_timestamp": "2017-05-25T19:53:36.737176", "_context_user_domain": null, "_context_user_name": null, "method": "object_action", "_context_remote_address": null}`
+
+	s := `{foo,bar,"hello,world","you|me"{`
+	
+	_, toks := token.Tokenise_qsep( s, "{}" )
+	for i, v := range toks {
+		fmt.Fprintf( os.Stderr, "[%d] %s\n", i, v )
+	}
+}
+
